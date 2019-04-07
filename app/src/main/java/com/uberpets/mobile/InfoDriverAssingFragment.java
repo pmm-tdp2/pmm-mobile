565db6d50e996bb2c63bcc420aad05d0e597ebb8
ruben <rbnm.jimenez@gmail.com>
2019-04-07 02:20:00 -0300

    fix info driver and adding to flow

--- app/src/main/java/com/uberpets/mobile/InfoDriverAssingFragment.java
+++ app/src/main/java/com/uberpets/mobile/InfoDriverAssingFragment.java
@@ -19,7 +19,6 @@ public class InfoDriverAssingFragment extends Fragment {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
