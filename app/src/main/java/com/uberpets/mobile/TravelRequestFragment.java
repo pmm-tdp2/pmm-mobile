48616e35366f8d3e753a0df6308acf95653021fd
federico longhi <federico.longhi@cloudgaia.com>
2019-04-08 01:52:20 -0300

    agrego card de notificacion de viaje entrante

---
+++ app/src/main/java/com/uberpets/mobile/TravelRequestFragment.java
@@ -0,0 +1,100 @@
+package com.uberpets.mobile;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link TravelRequestFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link TravelRequestFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class TravelRequestFragment extends Fragment {
+    private static final String ARG_PARAM1 = "info";
+
+    // TODO: info should be info of the trip
+    private String info;
+
+    private OnFragmentInteractionListener mListener;
+
+    public TravelRequestFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @return A new instance of fragment TravelRequestFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static TravelRequestFragment newInstance(String info) {
+        TravelRequestFragment fragment = new TravelRequestFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, info);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            info = getArguments().getString(ARG_PARAM1);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_travel_request, container, false);
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+}
