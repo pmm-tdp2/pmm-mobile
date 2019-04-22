b94530cae8b0c994cf9f27ae8e6faeec828bb718
federico longhi <federico.longhi@cloudgaia.com>
2019-04-22 18:27:15 -0300

    checkbox mejorar servicio

--- app/src/main/java/com/uberpets/mobile/UserFinalScreen.java
+++ app/src/main/java/com/uberpets/mobile/UserFinalScreen.java
@@ -4,31 +4,42 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.CheckBox;
 import android.widget.RatingBar;
 
 public class UserFinalScreen extends AppCompatActivity {
 
     private RatingBar mRatingBar;
+    private CheckBox mCheckBox;
     private String TAG_RATING_USER = "USER_RATING_STARS";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_final_screen);
-        //mRatingBar = findViewById(R.id.ratingBar_user);
+        mRatingBar = findViewById(R.id.ratingBar_user);
+        mCheckBox = findViewById(R.id.betterServiceCheckbox);
+        mCheckBox.setVisibility(View.INVISIBLE);
+    }
+
+    @Override
+    public void onBackPressed(){
+        //TODO: ver si hacemos algo aca
     }
 
     public void sendComment(android.view.View view){
         //int numStars = mRatingBar.getNumStars();
         //if(numStars != 0) {
         //    Log.i(TAG_RATING_USER,"user: idUser "+ " has scored with "+numStars );
-            //go back to activity that called it
-            finish();
+        //go back to activity that called it
+        finish();
         //}
     }
 
-    @Override
-    public void onBackPressed(){
-        //TODO: ver si hacemos algo aca
+    public void onRatingBarChange(android.view.View view){
+        int rating = mRatingBar.getNumStars();
+        if (rating <= 3) mCheckBox.setVisibility(View.VISIBLE);
+        else mCheckBox.setVisibility(View.INVISIBLE);
     }
 }
